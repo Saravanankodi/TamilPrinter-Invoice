4af00264f68c9ac0fd1a843e126d0280b76b6244
haran2005 <hariharan.srk2005@gmail.com>
2026-02-04 18:36:30 +0530

    created:components

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Print } from '@/assets/icons'
+import { DashBoard, Print } from '@/assets/icons'
 import React from 'react'
 
 const Sidebar = () => {
@@ -6,8 +6,8 @@ const Sidebar = () => {
     <>
     <section className="w-auto max-w-65 h-full p-2">
         <aside className="w-full flex items-center justify-center gap-2">
-            <div className="w-8 h-fit flex items-center justify-center bg-[#0496ff] ">
-                <Print className=' m-auto  '/>
+            <div className="w-6 h-fit flex items-center justify-center bg-[#0496ff] ">
+                <DashBoard className=''/>
             </div>
             <h2 className="text-2xl text-center">
                 Tamil Printers POS
